f11e4c0793e30c1ff4c2987a71e74296d4c177c1
Tim Abell <tim@timwise.co.uk>
2022-06-11 16:28:00 +0100

    Stub subcommand parsing
    https://stackoverflow.com/questions/71991935/how-to-make-a-default-subcommand-with-clap-and-derive/71992418#71992418

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 /// gitopolis, a cli tool for managnig multiple git repositories - https://github.com/timabell/gitopolis/#readme
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[clap(author, version)]
 struct Args {
+	#[clap(subcommand)]
+	command: Option<Commands>,
+}
 
-	#[clap(short, long)]
-	foo: String,
+#[derive(Subcommand)]
+enum Commands {
+	/// add one or more git repos to manage
+	Add { value: Option<String> },
 }
 
 fn main() {
 	let args = Args::parse();
 
-	println!("your arg was: {}", args.foo)
+	match &args.command {
+		Some(Commands::Add { value }) => {
+			println!("add command received with arg {:?}", value);
+		}
+		None =>{
+			println!("nada");
+		}
+	}
 }
