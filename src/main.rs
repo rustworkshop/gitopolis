b436a36da7bef7a1452ff37b26c8d3ea3078681a
Tim Abell <tim@timwise.co.uk>
2022-06-11 15:56:08 +0100

    Stub argument parsing
    https://github.com/clap-rs/clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+/// gitopolis, a cli tool for managnig multiple git repositories - https://github.com/timabell/gitopolis/#readme
+#[derive(Parser, Debug)]
+#[clap(author, version)]
+struct Args {
+
+	#[clap(short, long)]
+	foo: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+	let args = Args::parse();
+
+	println!("your arg was: {}", args.foo)
 }
