25a237b9140e829b6046962b24501860edd14db2
Tim Abell <tim@timwise.co.uk>
2022-07-16 23:35:26 +0100

    Move exec to own module

---
+++ src/exec.rs
@@ -0,0 +1,23 @@
+use crate::Repos;
+use std::process::Command;
+
+pub fn exec(mut exec_args: Vec<String>, repos: &Repos) {
+	let args = exec_args.split_off(1);
+	let cmd = &exec_args[0]; // only cmd remaining after split_off above
+	for repo in &repos.repos {
+		repo_exec(&repo.path, &cmd, &args);
+	}
+}
+
+fn repo_exec(path: &str, cmd: &str, args: &Vec<String>) {
+	println!("🌲 {}> {} {:?}", path, cmd, args);
+	let output = Command::new(cmd)
+		.args(args)
+		.current_dir(path)
+		.output()
+		.expect(&format!("Error running exec {}", cmd));
+
+	let stdout = String::from_utf8(output.stdout).expect("Error converting stdout to string");
+	println!("{}", stdout);
+	println!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use clap::{Parser, Subcommand};
-use std::process::Command;
 mod repos;
 use repos::*;
+mod exec;
 mod storage;
+use exec::*;
+
 use log::LevelFilter;
 use std::io::Write;
 use storage::*;
@@ -80,27 +82,6 @@ fn main() {
 			panic!("no command") // this doesn't happen because help shows instead
 		}
 	}
-}
-
-fn exec(mut exec_args: Vec<String>, repos: &Repos) {
-	let args = exec_args.split_off(1);
-	let cmd = &exec_args[0]; // only cmd remaining after split_off above
-	for repo in &repos.repos {
-		repo_exec(&repo.path, &cmd, &args);
-	}
-}
-
-fn repo_exec(path: &str, cmd: &str, args: &Vec<String>) {
-	println!("🌲 {}> {} {:?}", path, cmd, args);
-	let output = Command::new(cmd)
-		.args(args)
-		.current_dir(path)
-		.output()
-		.expect(&format!("Error running exec {}", cmd));
-
-	let stdout = String::from_utf8(output.stdout).expect("Error converting stdout to string");
-	println!("{}", stdout);
-	println!();
 }
 
 fn list(repos: &Repos) {
