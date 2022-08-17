96dbae4c936a519bedcbfaa34760dabf57db9c8e
Tim Abell <tim@timwise.co.uk>
2022-08-17 22:47:38 +0100

    Add test for add_repo()
    Tried just saving saved string in FakeStorage but that caused save to
    need a mutable self reference so abandoned that and went for setting a
    callback thus avoiding the need to mutate the struct after it's created.
    
    Guided by this incredible stackoverflow answer on creating callbacks:
    https://stackoverflow.com/questions/41081240/idiomatic-callbacks-in-rust/41081702#41081702

--- tests/gitopolis_tests.rs
+++ tests/gitopolis_tests.rs
@@ -1,26 +1,28 @@
 use gitopolis::gitopolis::Gitopolis;
+use gitopolis::repos::Repos;
 use gitopolis::storage::Storage;
 
 #[test]
-fn read() {
-	let g = Gitopolis::new(Box::new(FakeStorage {}));
-	let r = g.read();
-	assert_eq!(3, r.repos.len())
+fn add_repo() {
+	let expected_toml = "[[repos]]
+path = \"foo\"
+tags = []
+";
+	let mut gitopolis = Gitopolis::new(Box::new(FakeStorage {
+		exists: false,
+		contents: "".to_string(),
+		file_saved_callback: Box::new(|state| assert_eq!(expected_toml.to_owned(), state)),
+	}));
+	let mut folders = Vec::new();
+	folders.push("foo".to_string());
+	gitopolis.add(&folders);
 }
 
-struct FakeStorage {}
-
-impl Storage for FakeStorage {
-	fn exists(&self) -> bool {
-		true
-	}
-
-	fn save(&self, _state_toml: String) {
-		todo!()
-	}
-
-	fn read(&self) -> String {
-		"[[repos]]
+#[test]
+fn read() {
+	let gitopolis = Gitopolis::new(Box::new(FakeStorage {
+		exists: true,
+		contents: "[[repos]]
 path = \"foo\"
 tags = [\"red\"]
 
@@ -31,6 +33,29 @@ tags = [\"red\"]
 [[repos]]
 path = \"baz aroony\"
 tags = []"
-			.to_string()
+			.to_string(),
+		file_saved_callback: Box::new(|_| {}),
+	}));
+	let r = gitopolis.read();
+	assert_eq!(3, r.repos.len())
+}
+
+struct FakeStorage {
+	exists: bool,
+	contents: String,
+	file_saved_callback: Box<dyn Fn(String)>,
+}
+
+impl Storage for FakeStorage {
+	fn exists(&self) -> bool {
+		self.exists
+	}
+
+	fn save(&self, state_toml: String) {
+		(self.file_saved_callback)(state_toml);
+	}
+
+	fn read(&self) -> String {
+		self.contents.to_owned()
 	}
 }
