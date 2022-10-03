46e006d431d7568f6cd7e8c6499359ac2178ad10
Tim Abell <tim@timwise.co.uk>
2022-10-03 23:02:36 +0100

    Refactor tests - fluent interface etc
    Add a fluent interface (builder pattern style) for creating fake storage
    and git. This pattern makes it easier to add more capabilities without
    making every usage know about every possible variation.
    
    Pulled starting/expected state strings to start of tests for
    readability.
    
    General tidy up.
    
    I'm not sure what real rust coders would make of .boxed() but it seems
    more readable to me at the moment by virtue of being less wordy.

--- tests/gitopolis_tests.rs
+++ tests/gitopolis_tests.rs
@@ -11,72 +11,77 @@ tags = []
 name = \"origin\"
 url = \"git://example.org/test_url\"
 ";
-	let mut gitopolis = Gitopolis::new(
-		Box::new(FakeStorage {
-			exists: false,
-			contents: "".to_string(),
-			file_saved_callback: Box::new(|state| assert_eq!(expected_toml.to_owned(), state)),
-		}),
-		Box::new(FakeGit {
-			clone_callback: Box::new(|_, _| {}),
-		}),
-	);
+	let storage = FakeStorage::new()
+		.with_file_saved_callback(|state| assert_eq!(expected_toml.to_owned(), state))
+		.boxed();
+	let git = FakeGit::new().boxed();
+	let mut gitopolis = Gitopolis::new(storage, git);
 	let mut folders = Vec::new();
 	folders.push("test_repo".to_string());
+
 	gitopolis.add(&folders);
 }
 
 #[test]
 fn read() {
-	let gitopolis = Gitopolis::new(
-		Box::new(FakeStorage {
-			exists: true,
-			contents: "[[repos]]
+	let starting_state = "[[repos]]
 path = \"test_repo\"
 tags = []
 [repos.remotes.origin]
 name = \"origin\"
 url = \"git://example.org/test_url\"\
-"
-			.to_string(),
-			file_saved_callback: Box::new(|_| {}),
-		}),
-		Box::new(FakeGit {
-			clone_callback: Box::new(|_, _| {}),
-		}),
-	);
-	let r = gitopolis.read();
-	assert_eq!(1, r.repos.len())
+";
+
+	let storage = FakeStorage::new()
+		.with_contents(starting_state.to_string())
+		.boxed();
+
+	let git = FakeGit::new().boxed();
+	let gitopolis = Gitopolis::new(storage, git);
+	let actual_repos = gitopolis.read();
+
+	let expected_repos = 1;
+	assert_eq!(expected_repos, actual_repos.repos.len())
 }
 
 #[test]
 fn clone() {
 	// todo: test cloning more than one repo
-	let gitopolis = Gitopolis::new(
-		Box::new(FakeStorage {
-			exists: true,
-			contents: "[[repos]]
+
+	let starting_state = "[[repos]]
 path = \"test_repo\"
 tags = []
 [repos.remotes.origin]
 name = \"origin\"
 url = \"git://example.org/test_url\"\
-"
-			.to_string(),
-			file_saved_callback: Box::new(|_| {}),
-		}),
-		Box::new(FakeGit {
-			clone_callback: Box::new(|actual_path, actual_url| {
-				assert_eq!(actual_path, "test_repo");
-				assert_eq!(actual_url, "git://example.org/test_url");
-			}),
-		}),
-	);
+";
+
+	let storage = FakeStorage::new()
+		.with_contents(starting_state.to_string())
+		.boxed();
+
+	let git = FakeGit::new()
+		.with_clone_callback(Box::new(|actual_path, actual_url| {
+			assert_eq!(actual_path, "test_repo");
+			assert_eq!(actual_url, "git://example.org/test_url");
+		}))
+		.boxed();
+
+	let gitopolis = Gitopolis::new(storage, git);
+
 	gitopolis.clone();
 }
 
 #[test]
 fn tag() {
+	let starting_state = "[[repos]]
+path = \"test_repo\"
+tags = []
+[repos.remotes.origin]
+name = \"origin\"
+url = \"git://example.org/test_url\"\
+";
+
 	let expected_toml = "[[repos]]
 path = \"test_repo\"
 tags = [\"some_tag\"]
@@ -84,23 +89,15 @@ tags = [\"some_tag\"]
 name = \"origin\"
 url = \"git://example.org/test_url\"
 ";
-	let mut gitopolis = Gitopolis::new(
-		Box::new(FakeStorage {
-			exists: true,
-			contents: "[[repos]]
-path = \"test_repo\"
-tags = []
-[repos.remotes.origin]
-name = \"origin\"
-url = \"git://example.org/test_url\"\
-"
-			.to_string(),
-			file_saved_callback: Box::new(|state| assert_eq!(expected_toml.to_owned(), state)),
-		}),
-		Box::new(FakeGit {
-			clone_callback: Box::new(|_, _| {}),
-		}),
-	);
+
+	let storage = FakeStorage::new()
+		.with_contents(starting_state.to_string())
+		.with_file_saved_callback(|state| assert_eq!(expected_toml.to_owned(), state))
+		.boxed();
+
+	let git = FakeGit::new().boxed();
+	let mut gitopolis = Gitopolis::new(storage, git);
+
 	gitopolis.add_tag("some_tag", &vec!["test_repo".to_string()]);
 }
 
@@ -108,6 +105,35 @@ struct FakeStorage {
 	exists: bool,
 	contents: String,
 	file_saved_callback: Box<dyn Fn(String)>,
+}
+
+// fluent interface for building up fake storage (like the "builder pattern")
+impl FakeStorage {
+	fn new() -> FakeStorage {
+		FakeStorage {
+			exists: false,
+			contents: "".to_string(),
+			file_saved_callback: Box::new(|_| {}),
+		}
+	}
+
+	fn with_contents(mut self, contents: String) -> Self {
+		self.exists = true;
+		self.contents = contents;
+		self
+	}
+
+	fn with_file_saved_callback<F>(mut self, callback: F) -> Self
+	where
+		F: Fn(String) + 'static, // todo: would it be possible to shrink lifetime from static?
+	{
+		self.file_saved_callback = Box::new(callback);
+		self
+	}
+
+	fn boxed(self) -> Box<dyn Storage> {
+		Box::new(self)
+	}
 }
 
 impl Storage for FakeStorage {
@@ -126,6 +152,24 @@ impl Storage for FakeStorage {
 
 struct FakeGit {
 	clone_callback: Box<dyn Fn(String, String)>,
+}
+
+// fluent interface for building up fake git
+impl FakeGit {
+	fn new() -> Self {
+		FakeGit {
+			clone_callback: Box::new(|_, _| {}),
+		}
+	}
+
+	fn with_clone_callback(mut self, callback: Box<dyn Fn(String, String)>) -> Self {
+		self.clone_callback = callback;
+		self
+	}
+
+	fn boxed(self) -> Box<Self> {
+		Box::new(self)
+	}
 }
 
 impl Git for FakeGit {
