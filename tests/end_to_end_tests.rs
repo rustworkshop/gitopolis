a22c8a9dbd5cae8fbd27d235840dfecb63549125
Tim Abell <tim@timwise.co.uk>
2022-11-17 22:53:53 +0000

    Add end-to-end testing of binary CLI
    Couple of rough tests to try out the concepts needed.
    
    References:
    * https://rust-cli.github.io/book/tutorial/testing.html
    * https://tekshinobi.com/temporary-files-and-temporary-directory-in-rust/
    * https://doc.rust-lang.org/std/env/fn.set_current_dir.html
    * https://doc.rust-lang.org/std/fs/fn.create_dir_all.html

---
+++ tests/end_to_end_tests.rs
@@ -0,0 +1,53 @@
+use assert_cmd::Command as AssertCommand;
+use predicates::prelude::predicate;
+use std::env::set_current_dir;
+use std::fs;
+use std::path::PathBuf;
+use std::process::Command;
+use tempfile::tempdir;
+
+#[test]
+fn help() {
+	let mut cmd = get_binary_cmd();
+	cmd.arg("help");
+	cmd.assert()
+		.success()
+		.stdout(predicate::str::contains("Usage: gitopolis"));
+}
+
+#[test]
+fn list_empty_exit_code_2() {
+	let mut cmd = get_binary_cmd();
+	cmd.arg("list");
+	cmd.assert()
+		.failure()
+		.code(2)
+		.stdout(predicate::str::contains("No repos"));
+}
+
+#[test]
+fn add() {
+	let temp = tempdir().expect("get tmp dir failed");
+	let repo = "some_git_folder";
+	init_repo(temp.path().join(repo));
+	set_current_dir(temp.path()).expect("chdir failed");
+
+	get_binary_cmd()
+		.args(vec!["add", repo])
+		.assert()
+		.success()
+		.stderr(predicate::str::contains("Added some_git_folder\n"));
+}
+
+fn init_repo(path: PathBuf) {
+	fs::create_dir_all(&path).expect("create repo dir failed");
+	set_current_dir(path).expect("chdir failed");
+	Command::new("git")
+		.args(vec!["init"])
+		.output()
+		.expect("git command failed");
+}
+
+fn get_binary_cmd() -> AssertCommand {
+	AssertCommand::cargo_bin("gitopolis").expect("failed to find binary")
+}
