2bcac3849c4d6126aa2c7891ef3a42823fd6141f
Tim Abell <tim@timwise.co.uk>
2022-12-18 19:35:40 +0000

    Pass errors up to caller
    https://www.sheshbabu.com/posts/rust-error-handling/

--- src/exec.rs
+++ src/exec.rs
@@ -1,25 +1,23 @@
 use crate::repos::Repo;
+use std::io::Error;
 use std::process::Command;
 
 pub fn exec(mut exec_args: Vec<String>, repos: Vec<Repo>) {
 	let args = exec_args.split_off(1);
 	let cmd = &exec_args[0]; // only cmd remaining after split_off above
 	for repo in &repos {
-		repo_exec(&repo.path, &cmd, &args);
+		repo_exec(&repo.path, &cmd, &args).expect(&format!("Error running exec {}", cmd));
 	}
 }
 
-fn repo_exec(path: &str, cmd: &str, args: &Vec<String>) {
+fn repo_exec(path: &str, cmd: &str, args: &Vec<String>) -> Result<(), Error> {
 	println!("🏢 {}> {} {}", path, cmd, args.join(" "));
-	let output = Command::new(cmd)
-		.args(args)
-		.current_dir(path)
-		.output()
-		.expect(&format!("Error running exec {}", cmd));
+	let output = Command::new(cmd).args(args).current_dir(path).output()?;
 
 	let stdout = String::from_utf8(output.stdout).expect("Error converting stdout to string");
 	let stderr = String::from_utf8(output.stderr).expect("Error converting stderr to string");
 	println!("{}", stdout);
 	println!("{}", stderr);
 	println!();
+	Ok(())
 }
