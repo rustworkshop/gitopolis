6d89f58b241e096a3be5e82acf1669630b7b0076
Tim Abell <tim@timwise.co.uk>
2023-04-13 16:17:44 +0100

    Avoid working-directory race in end-to-end tests
    `env::set_current_dir()` can't be used in parallel test runs as it's process-global and causes the tests to end up in each others temp test directories.
    
    https://users.rust-lang.org/t/race-condition-in-file-creation/7991

--- tests/end_to_end_tests.rs
+++ tests/end_to_end_tests.rs
@@ -1,6 +1,5 @@
 use assert_cmd::Command as AssertCommand;
 use predicates::prelude::predicate;
-use std::env::set_current_dir;
 use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
@@ -28,19 +27,20 @@ fn list_empty_exit_code_2() {
 #[test]
 fn add() {
 	let temp = tempdir().expect("get tmp dir failed");
-	let repo = "some_git_folder";
+	let repo = "add_some_git_folder";
 	init_repo(temp.path().join(repo), "git://example.org/test_url");
-	set_current_dir(temp.path()).expect("chdir failed");
 
 	get_binary_cmd()
+		.current_dir(&temp)
 		.args(vec!["add", repo])
 		.assert()
 		.success()
-		.stderr(predicate::str::contains("Added some_git_folder\n"));
+		.stderr(predicate::str::contains("Added add_some_git_folder\n"));
 
-	let actual_toml = fs::read_to_string(".gitopolis.toml").expect("failed to read back toml");
+	let actual_toml =
+		fs::read_to_string(temp.path().join(".gitopolis.toml")).expect("failed to read back toml");
 	let expected_toml = "[[repos]]
-path = \"some_git_folder\"
+path = \"add_some_git_folder\"
 tags = []
 
 [repos.remotes.origin]
@@ -52,12 +52,13 @@ url = \"git://example.org/test_url\"
 
 fn init_repo(path: PathBuf, remote_url: &str) {
 	fs::create_dir_all(&path).expect("create repo dir failed");
-	set_current_dir(path).expect("chdir failed");
 	Command::new("git")
+		.current_dir(&path)
 		.args(vec!["init"])
 		.output()
 		.expect("git command failed");
 	Command::new("git")
+		.current_dir(&path)
 		.args(vec!["config", "remote.origin.url", remote_url])
 		.output()
 		.expect("git command failed");
