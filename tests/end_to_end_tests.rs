865678c9f3b4376afc11acb6693cb9eb02b0eb11
Tim Abell <tim@timwise.co.uk>
2023-04-13 16:18:05 +0100

    Add end to end tests for list and tag

--- tests/end_to_end_tests.rs
+++ tests/end_to_end_tests.rs
@@ -50,6 +50,76 @@ url = \"git://example.org/test_url\"
 	assert_eq!(expected_toml, actual_toml);
 }
 
+#[test]
+fn tag() {
+	let temp = tempdir().expect("get tmp dir failed");
+	let repo = "tag_some_git_folder";
+	init_repo(temp.path().join(repo), "git://example.org/test_url");
+
+	get_binary_cmd()
+		.current_dir(&temp)
+		.args(vec!["add", repo])
+		.assert()
+		.success()
+		.stderr(predicate::str::contains("Added tag_some_git_folder\n"));
+
+	get_binary_cmd()
+		.current_dir(&temp)
+		.args(vec!["tag", "some_tag", repo])
+		.assert()
+		.success();
+
+	let actual_toml =
+		fs::read_to_string(temp.path().join(".gitopolis.toml")).expect("failed to read back toml");
+	let expected_toml = "[[repos]]
+path = \"tag_some_git_folder\"
+tags = [\"some_tag\"]
+
+[repos.remotes.origin]
+name = \"origin\"
+url = \"git://example.org/test_url\"
+";
+	assert_eq!(expected_toml, actual_toml);
+}
+
+#[test]
+fn list() {
+	let temp = tempdir().expect("get tmp dir failed");
+	let repo = "list_some_git_folder";
+	init_repo(temp.path().join(repo), "git://example.org/test_url");
+
+	get_binary_cmd()
+		.current_dir(&temp)
+		.args(vec!["add", repo])
+		.assert()
+		.success();
+
+	get_binary_cmd()
+		.current_dir(&temp)
+		.args(vec!["list"])
+		.assert()
+		.success()
+		.stdout(predicate::str::contains("list_some_git_folder"));
+
+	get_binary_cmd()
+		.current_dir(&temp)
+		.args(vec!["list", "-l"])
+		.assert()
+		.success()
+		.stdout(predicate::str::contains(
+			"list_some_git_folder\t\tgit://example.org/test_url",
+		));
+
+	get_binary_cmd()
+		.current_dir(&temp)
+		.args(vec!["list", "--long"])
+		.assert()
+		.success()
+		.stdout(predicate::str::contains(
+			"list_some_git_folder\t\tgit://example.org/test_url",
+		));
+}
+
 fn init_repo(path: PathBuf, remote_url: &str) {
 	fs::create_dir_all(&path).expect("create repo dir failed");
 	Command::new("git")
