88bd6de63278371a6dcd76b97a8982cf330d4cef
Tim Abell <tim@timwise.co.uk>
2023-10-20 22:09:03 +0100

    Allow upgrading to "incompatible" crates
    As I understand it this means upgrade major and minor versions as well
    as patch versions, which could have breaking changes. Our tests will
    protect us so we can keep up to date.

--- upgrade-crates.sh
+++ upgrade-crates.sh
@@ -2,6 +2,6 @@
 set -e # exit on error
 cargo update
 cargo install cargo-edit
-cargo upgrade # from cargo-edit
+cargo upgrade --incompatible # from cargo-edit
 cargo test
 git commit -i Cargo.lock -i Cargo.toml -m "cargo update/upgrade"
