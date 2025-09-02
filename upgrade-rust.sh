9b263bba512c184f3cfb9e3f738fc20767d78e9f
Tim Abell <t@0x5.uk>
2025-09-02 19:09:27 +0100

    Adjust upgrade-rust for asdf v0.18
    Latest asdf has different behaviour:
    - new line on end of version list, so take 2 lines
    - local command gone, replaced with set
    
    Seems to now show up with a blank line on the end of the list
    
    https://github.com/timabell/sln-items-sync/commit/81c74dedda5ceb2a92b7a5387ec4b746fd5bed17

--- upgrade-rust.sh
+++ upgrade-rust.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -v
 set -e # exit on error
 asdf plugin update rust
-latest=`asdf list all rust | tail -n 1`
+latest=`asdf list all rust | tail -n 2`
 echo $latest
 asdf install rust $latest
-asdf local rust $latest
+asdf set rust $latest
 cargo test
 git commit -i .tool-versions -m "Upgrade rust to latest"
